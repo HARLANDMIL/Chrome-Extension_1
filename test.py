bd031fb02a363ca59c154da00c66d2620d176ec0
MILTON HARLAND <harlandmilton06@gmail.com>
2024-07-30 13:34:26 +0200

    Add a new test file

---
+++ test.py
@@ -0,0 +1,12 @@
+fave_language = "Python"
+
+print("I like coding in " + fave_language + " the most")
+
+
+my_name_is= "Milton Harland",
+age = 25,
+city = "New York",
+country = "USA",
+fave_language = "Python",
+
+print("My name is " + my_name_is + " and I am " + country)
